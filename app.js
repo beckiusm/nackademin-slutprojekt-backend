bbf596e62c399ca1ed022528d4f5aa26163da411
Mathias Beckius <lillmatte@hotmail.com>
2020-09-18 13:43:41 +0200

    fixade lite fel

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const app = express()
 app.use( express.static('public') )
 
 // router
-const orderRouter = require('./routes/order');
-const userRouter = require('./routes/items');
-const productRouter = require('./routes/users');
-app.use('/api/orders', orderRouter);
-app.use('/api/users', userRouter);
-app.use('/api/products', productRouter);
+// const orderRouter = require('./routes/order');
+// const userRouter = require('./routes/items');
+// const productRouter = require('./routes/users');
+// app.use('/api/orders', orderRouter);
+// app.use('/api/users', userRouter);
+// app.use('/api/products', productRouter);
 
 module.exports = app
--- start.js
+++ start.js
@@ -1,5 +1,6 @@
 const app = require('./app')
 const db = require('./database/db');
+const port = process.env.PORT || 3000;
 
 db.connect().then( () => 
 	app.listen(port, () => console.log(`Running app on port ${port}`))
